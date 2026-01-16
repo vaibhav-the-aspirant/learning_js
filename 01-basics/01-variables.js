e76fb075bc072714138226c95f55aca072d02512
vaibhav-the-aspirant <vaibhav9415258604@gmail.com>
2026-01-16 23:58:55 +0000

    Explored variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,16 @@
+console.log("Let's start learning JS")
+
+const accountId=23233
+let accountemail="vaib@gmail.com"
+var accountPassword="12345"
+accountCity= "Delhi"
+let accountState
+
+// accountId=2
+
+accountemail="hc@gmail.com"
+accountPassword="09876"
+accountCity= "Lucknow"
+
+console.table([accountId,accountemail,accountPassword,accountCity,accountState])
+
