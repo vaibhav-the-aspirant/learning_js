9e55930d9e1aa6fe7fcb655eed77cf94e46c4956
vaibhav-the-aspirant <vaibhav9415258604@gmail.com>
2026-01-17 13:30:37 +0000

    read about confusing datatype conversions

---
+++ 01-basics/04-conversionOperations.js
@@ -0,0 +1,47 @@
+// let num=44
+// let negNum= -num
+
+// console.log(num)  //44
+// console.log(typeof num)  //number
+
+// console.log(negNum)  //-44
+// console.log(typeof negNum)  //number
+
+let str1= "JS"
+let str2= " mein aisa hi hota h"
+let str3= str1+str2
+console.log(str3);  //Js mein aisa hi hota h
+
+console.log(1 + "2")    //12
+console.log("1" + 2)    //12
+console.log("1" + 2 + 2)  //122
+console.log(1 + 2 + "2")  //32 ,waah re js!
+
+
+// console.log(+true) // 1
+// console.log(true+) // error
+
+// console.log(+"") //0
+// console.log(""+) // error
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 //generally this confusing style is avoided
+
+let gameCounter=100
+++gameCounter   //prefix
+console.log(gameCounter)    //101
+
+
+gameCounter++   //postfix
+console.log(gameCounter)    //102
+
+
+
+
+
+
+
+
+
+
+
