74e2e945f91292889376c612b989fd7a2ff7ed9c
vaibhav-the-aspirant <vaibhav9415258604@gmail.com>
2026-01-17 00:46:42 +0000

    Explored datatypes

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,35 @@
+"use strict" //treat all my js code as newer version of js
+
+// alert(3+3) can't use alert like this in nodeJS, works well in broswer though
+
+console.log(3
+    +
+    3)  // it will run properly as JS automatically ignore extra white spaces. But this is not a good prctice as it kills readability.
+
+console.log(2+8); 
+
+console.log("vaibhav")
+
+let number = 100
+let name= "vaibhav"
+let isLoggedIn= false
+
+/* data types - 
+    number -- 
+    big int
+    string -- " "
+    boolean - true/false
+    null - standalone value
+    undefined
+    symbol => unique
+
+    OBJECT
+*/
+
+console.log(typeof null); // expected output => object
+console.log(typeof undefined); // expected output => undefined
+
+
+
+
+    
