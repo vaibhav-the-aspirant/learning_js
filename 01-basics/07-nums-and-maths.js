c612ead89ae46c306279bdd7968f859c56a811ec
vaibhav-the-aspirant <vaibhav9415258604@gmail.com>
2026-01-17 21:44:34 +0000

    about numbers and maths

---
+++ 01-basics/07-nums-and-maths.js
@@ -0,0 +1,28 @@
+// const score = 900
+// console.log(score); // => 900
+
+// const numID = new Number(300)
+// console.log(numID); // => [Number: 300]
+
+// console.log(numID.toString())
+// console.log(numID.toFixed(2)) // => 300.00
+
+// const anotherNumber = 10.9867
+// console.log(anotherNumber.toPrecision(3)) // => 11.0
+
+// const num1 = 6.4751
+// console.log(num1.toPrecision(3)) // =>  6.48
+
+// const hundrads = 1000000
+// console.log(hundrads.toLocaleString('en-IN'))
+
+//----------------------------------------- Maths ---------------------------------------------------------------------
+
+const min = 10
+const max = 20
+console.log(Math.random())
+console.log(Math.random()*10 + 1)
+console.log(Math.floor(Math.random()*10 + 1))
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)  //IMPORTANT FORMULA.
