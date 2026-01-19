743d858453f515f7896bba78aa460c09f845c4fb
vaibhav-the-aspirant <vaibhav9415258604@gmail.com>
2026-01-19 00:13:15 +0000

    more on scopes

--- 03-basics/03-scopes.js
+++ 03-basics/03-scopes.js
@@ -4,10 +4,36 @@ if (true){
     let a = 10
     const b = 20
     var c = 30
-console.log(a);
+// console.log(a);
     
 }
 
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b)
+// console.log(c);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+function one(){
+    const userName = "vaibhav"
+    function two(){
+        const website = "utube"
+        console.log(userName);
+    }
+    two()
+    // console.log(website)
+}
+// one()   
+
+// ------------------------------INTERESTING------------------------------------------------------------------------------
+
+console.log(addOne(3))  //allowed - no error
+function addOne(num){
+    return num+1
+}
+
+addtwo(8) // throws error - ReferenceError: Cannot access 'addtwo' before initialization
+const addtwo = function (num){
+    return num+2
+}
+
