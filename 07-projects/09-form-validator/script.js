d3708284ba0889cb7ff78f3bc126a3b266a7bc27
vaibhav-the-aspirant <vaibhav9415258604@gmail.com>
2026-01-26 06:09:13 +0530

    added email validation

--- 07-projects/09-form-validator/script.js
+++ 07-projects/09-form-validator/script.js
@@ -79,20 +79,63 @@ function hasUnsufficientLength(str){
 
 //----------------------------------------------------------------
 
+
 //email ke declarations
-
-
-
-
-
+const email = document.querySelector('#email')
+const emailAnnotation = document.querySelector('#emailAnnotation')
+const emailDot= document.querySelector('#emailDot')
+const emailOrder = document.querySelector('#emailOrder')
 
 
 //email ka event listener 
+email.addEventListener('input',(e)=>{
+    const value = e.target.value;
+    let Annotation = false
 
+    // '@' hai ki nhi check krenge phle
+    Annotation = hasAnnotation(value)
+    //if no -> display error msg
+    if(Annotation){
+        emailAnnotation.innerHTML = ""
+    } else {
+        emailAnnotation.innerHTML = "Email must contain '@'"
+    }
 
-
-
-
-
+    let Dot = false
+    // '.' hai ki nhi check krenge
+    Dot = hasDot(value)
+    //if no -> display error msg
+    if(Dot){
+        emailDot.innerHTML = ''
+    }
+    else {
+        emailDot.innerHTML = "Email must contain '.'"
+    }
+    
+    let order = false;
+    // '@' dot se phle aa rha ki nhi iske liye check
+    order = hasCorrectOrder(value)
+    if(order){
+        emailOrder.innerHTML = ""
+    }else {
+        emailOrder.innerHTML = "'@' must come before '.' "
+    }
+})
 
 //email vaildations ke liye functions
+function hasAnnotation(str){
+    return str.includes('@')
+}
+function hasDot(str){
+    return str.includes('.')
+}
+function hasCorrectOrder(str){
+    if (!hasAnnotation(str) || !hasDot(str)) {
+        return true; // order not applicable yet
+    }
+    return str.indexOf('@') < str.indexOf('.');
+}
+
+
+//-----------------------------------------------------------------
+
